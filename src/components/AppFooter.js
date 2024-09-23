5dd05f14d4dee684d1b8d8f2b22f8cee0fd87028
12malak <alfarwanmalak@gmail.com>
2024-09-23 22:52:49 +0300

    edat

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -60,14 +60,14 @@ function AppFooter() {
             <IoIosArrowRoundForward 
                 size="1.5rem" 
                 className="position-absolute" 
-                style={{ left: '10px', top: '50%', transform: 'translateY(-50%)', color: '#6c757d' }} 
+                style={{ right: '10px', top: '50%', transform: 'translateY(-50%)', color: '#6c757d' }} 
             />
             <Form.Control 
                 name="email" 
                 type="email" 
                 placeholder="Email" 
                 required 
-                style={{ paddingLeft: '40px' }} // Adjust padding to prevent text from overlapping the icon
+              
             />
         </Form.Group>
                     </Col>
